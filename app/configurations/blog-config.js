cbb2b7d7756643b9c18a2174ec26369aa4e3543a
vickie-ks <vigneshrajan2022@gmail.com>
2024-05-22 01:42:21 -0400

    [RELEASE] Quest 2 released

--- app/configurations/blog-config.js
+++ app/configurations/blog-config.js
@@ -19,7 +19,7 @@ export let BLOG_ARTICLES = [
     <p>But today's proxies aren’t just about accessing the restricted or hiding in the shadows; they’re evolving. Imagine proxies as your personal bodyguards, or even better, shape-shifters, adapting in real-time to protect you from digital threats. They’re the silent sentinels in the war against cyber-attacks, the watchful guardians of network traffic.</p>
     <p>In the grand tapestry of the internet, proxies are both shield and sword, blending old roles with new innovations to ensure that every digital explorer can navigate safely and freely. So, as the digital winds shift, remember: <b>Only those who master the shadows can truly harness the light.</b> Embrace the power of proxies, and chart a fearless course through the cyber seas!<p>`,
     date: new Date('May 21 2024'),
-    imageUrl: 'images/blogs/proxy-sec.png'
+    imageUrl: 'images/blogs/proxy-sec.png',
   },
   {
     id: '1',
@@ -36,6 +36,19 @@ export let BLOG_ARTICLES = [
 ];
 
 export let QUEST_ARTICLES = [
+  {
+    id: '2',
+    title: 'Echoes of Our Ancestors ~ A Story of Connection',
+    description: `<p>Deep in Africa's lush jungles, there's a place called the "<b>Chimp Empire</b>," where the lives of chimpanzees are as rich and joyful as any story you've heard. This magical world, shown in the Netflix documentary, lets us peek into the lives of these amazing animals, our closest cousins in the wild.</p>
+    <p>Imagine a place where young chimps play freely, chasing each other under the shade of giant trees. Their laughter fills the air and warms the heart. Nearby, adult chimps sit together, grooming each other with gentle hands, showing love and care in every touch. These simple moments create strong bonds among them, much like the families we know.</p>
+    <p>Watching them more closely, we see how smart they are. Chimps make and use tools with ease. They can pick up a stick, clean it, and use it to get tasty bugs from inside a tree. They also use stones to break open nuts. This cleverness shows us how much we have in common with them.</p>
+    <p>As we learn about their lives, we see that chimps are a lot like us. They have <i>feelings</i>, they make <i>friends</i>, and they <i>look out for each other</i>. This connection goes back a long way, to the very early days of humans.</p>
+    <p>This story of the chimps is not just interesting—it also touches our hearts. It teaches us to care more about these wonderful animals and the forests they call home. By looking after them, we keep a part of our world safe and full of wonder.</p>
+    <p>Let's keep the story of the <b>Chimp Empire</b> alive. It teaches us about our past and shows us how caring for others, no matter how different, makes us all better.</p>
+    <p><b><i>In every chimp's smile and every playful chase, we find a piece of our shared story. Let's cherish and protect their world as they teach us about our own.</i></b></p>`,
+    date: new Date('May 22 2024'),
+    imageUrl: 'images/blogs/chimp-empire.png',
+  },
   {
     id: '1',
     title: 'The Journey of Life on Our Planet!',
